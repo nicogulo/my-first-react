cd31e524aee406444050425be53a20dc3b08d95f
nicogulo <nicogulobelas@gmail.com>
2021-01-08 15:24:58 +0700

    try Props and State

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+const name = "Nic";
+const element = <h1> Hello {name} </h1>;
+
+ReactDOM.render(
+  element,
+  document.getElementById('master')
+);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
